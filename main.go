ec32b33622481e5290a08f28daf4849fad0a028e
Yusuf Dogan <you@example.com>
2022-03-08 23:43:39 +0300

    changes

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 	models "ydgo/src/models"
 	service "ydgo/src/service"
 )
@@ -17,6 +18,7 @@ func main() {
 	server := http.Server{
 		Addr:    ":6868",
 		Handler: &myHandler{},
+		ReadTimeout: 5*time.Second,
 	}
 	mux = make(map[string]func(http.ResponseWriter, *http.Request))
 	mux["/question"] = gptQuestionAction
@@ -28,7 +30,6 @@ func main() {
 }
 
 func gptQuestionAction(w http.ResponseWriter, r *http.Request) {
-	r.Host = ""
 	var questionRequest models.QuestionRequest
 	err := json.NewDecoder(r.Body).Decode(&questionRequest)
 	if err != nil {
@@ -43,13 +44,13 @@ func gptQuestionAction(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(questionResponse)
-	r.Body.Close();
 }
 
 func (*myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	r.Host = ""
+	r.Host = "http://yusufdgn.com"
 	if h, ok := mux[r.URL.String()]; ok {
 		h(w, r)
+		r.Body.Close()
 		return
 	}
 	http.Error(w, "404 not found.", http.StatusNotFound)
